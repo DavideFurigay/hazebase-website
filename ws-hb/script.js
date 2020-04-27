ba45d9f9df328b2de873fdc271611cb9a2d86509
Davide Furigay <Davide.Furigay@swisscom.com>
2020-04-27 19:16:26 +0200

    tried to implement comments

--- ws-hb/script.js
+++ ws-hb/script.js
@@ -1,24 +1,43 @@
-var titles = [];
-var titleInput = document.getElementById("title");
-var messageBox = document.getElementById("display");
+//Comments
+$(function(){
+    $("#form").submit(function(e){
+        // Create data object
+        var data = {};
+        data.name = $("#name").val();
+        data.email = $("#email").val();
+        data.comment = $("#comment").val();
+        // The url of your server-side script that handles the post submission
+        var url = "http://www.someurl.com/post_comment.php";
+        $.POST(url, data)
+        .done(function(response){
+            // This code executes when the server returns a response
+            // Do something with the response like adding the comment to the current list of comments
+            // Example (if your response is HTML, better would be a JSON string):
+            $("#comments").append(response);
+        });
+        e.preventDefault();
+    });
+});
 
-function Allow()
-    {
-    if (!user.title.value.match(/[a-zA-Z]$/) && user.title.value !="")
-        {
-            user.title.value="";
-            alert("Please Enter only alphabets");
-        }
-        window.location.reload()
+//Live Search
+function showResult(str) {
+    if (str.length==0) {
+      document.getElementById("livesearch").innerHTML="";
+      document.getElementById("livesearch").style.border="0px";
+      return;
     }
-
-function insert () {
-    titles.push(titleInput.value);
-    clearAndShow();
-}
-
-function clearAndShow () {
-    titleInput.value = "";
-    messageBox.innerHTML = "";
-    messageBox.innerHTML += " " + titles.join("<br/> ") + "<br/>";
-}
+    if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp=new XMLHttpRequest();
+    } else {  // code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function() {
+      if (this.readyState==4 && this.status==200) {
+        document.getElementById("livesearch").innerHTML=this.responseText;
+        document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+      }
+    }
+    xmlhttp.open("GET","livesearch.php?q="+str,true);
+    xmlhttp.send();
+  }
